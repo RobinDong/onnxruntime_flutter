d1418a31eb6d18631dd772b7925218b31db40a89
Robin Dong <robin.k.dong@gmail.com>
2025-09-22 19:33:14 +1000

    Upgrade macOS/Linux of onnxruntime to onnxruntime-v1.22.2

--- lib/src/bindings/bindings.dart
+++ lib/src/bindings/bindings.dart
@@ -12,7 +12,7 @@ final DynamicLibrary _dylib = () {
   }
 
   if (Platform.isMacOS) {
-    return DynamicLibrary.open('libonnxruntime.1.19.2.dylib');
+    return DynamicLibrary.open('libonnxruntime.1.22.2.dylib');
   }
 
   if (Platform.isWindows) {
@@ -20,7 +20,7 @@ final DynamicLibrary _dylib = () {
   }
 
   if (Platform.isLinux) {
-    return DynamicLibrary.open('libonnxruntime.so.1.19.2');
+    return DynamicLibrary.open('libonnxruntime.so.1.22.2');
   }
 
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
